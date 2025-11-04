ee24f0b83c1a9e0bdf00dbbdbd6b15b6a0d2d0a2
neteyam340 <granjahervyn@gmail.com>
2025-11-04 01:12:52 -0500

    taller

---
+++ Report.java
@@ -0,0 +1,11 @@
+public class Report {
+    public void generateReport() {
+        // Lógica para generar el reporte
+        System.out.println("Reporte generado.");
+    }
+
+    public void saveToFile() {
+        // Lógica para guardar el reporte en un archivo
+        System.out.println("Reporte guardado en archivo.");
+    }
+}
